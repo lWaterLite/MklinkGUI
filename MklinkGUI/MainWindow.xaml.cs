88246a01d951c470b706c8489fd7a365c7b66fd8
lWaterLite <lwaterlite@126.com>
2024-02-20 20:18:38 +0800

    upload prj

---
+++ MklinkGUI/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Color = System.Drawing.Color;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
+using DialogResult = System.Windows.Forms.DialogResult;
+
+namespace MklinkGUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly MainWindowViewModel _mainWindowViewModel;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _mainWindowViewModel = (DataContext as MainWindowViewModel)!;
+        
+        CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand,
+            (_, _) => SystemCommands.CloseWindow(this)));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand,
+            (_, _) => SystemCommands.MinimizeWindow(this)));
+    }
+
+    #region PrivateMethod
+
+    private void SelectLinkPathButton(object sender, RoutedEventArgs eventArgs)
+    {
+        FolderBrowserDialog dialog = new();
+        if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+        _mainWindowViewModel.LinkPath = dialog.SelectedPath;
+    }
+
+    private void SelectTargetPathButton(object sender, RoutedEventArgs eventArgs)
+    {
+        if (_mainWindowViewModel.LinkMode == LinkMode.FileMode)
+        {
+            OpenFileDialog dialog = new()
+            {
+                Multiselect = false
+            };
+            if (dialog.ShowDialog() != true) return;
+            _mainWindowViewModel.TargetPath = dialog.FileName;
+        }
+        else
+        {
+            FolderBrowserDialog dialog = new();
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            _mainWindowViewModel.TargetPath = dialog.SelectedPath;
+        }
+    }
+
+    private async void ExecuteMklinkButton(object sender, RoutedEventArgs eventArgs)
+    {
+        if (_mainWindowViewModel.LinkPath == string.Empty || _mainWindowViewModel.TargetPath == string.Empty) return;
+        string param;
+        if (_mainWindowViewModel.LinkMode == LinkMode.FileMode)
+            param = _mainWindowViewModel.IsHardLink == false ? "" : "/H";
+        else
+            param = _mainWindowViewModel.IsJunction == false ? "/D" : "/J";
+
+        DirectoryInfo targetPathDirectoryInfo = new(_mainWindowViewModel.TargetPath);
+        string fullLinkPath = _mainWindowViewModel.LinkPath + "\\" + targetPathDirectoryInfo.Name;
+        string command = $"/c \"mklink {param} {fullLinkPath} {_mainWindowViewModel.TargetPath}\"";
+
+        ProcessStartInfo info = new()
+        {
+            FileName = "cmd.exe",
+            Arguments = command,
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+        StringBuilder error = new();
+
+        Process process = new();
+        process.StartInfo = info;
+        process.ErrorDataReceived += (_, e) =>
+        {
+            if (e.Data != null)
+                error.AppendLine(e.Data);
+        };
+
+        process.Start();
+        process.BeginErrorReadLine();
+
+        await process.WaitForExitAsync();
+        if (error.ToString() == string.Empty)
+        {
+            _mainWindowViewModel.ExecutionResult = "成功执行";
+            _mainWindowViewModel.ExecutionResultForeground = new SolidColorBrush(Colors.Chartreuse);
+        }
+        else
+        {
+            _mainWindowViewModel.ExecutionResult = "执行失败，错误输出如下\n" + error;
+            _mainWindowViewModel.ExecutionResultForeground = new SolidColorBrush(Colors.Red);
+        }
+    }
+
+    #endregion
+}
---
+++ MklinkGUI/MainWindowViewModel.cs
@@ -0,0 +1,174 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Brush = System.Windows.Media.Brush;
+
+namespace MklinkGUI;
+
+public enum LinkMode
+{
+    FileMode,
+    FolderMode
+}
+
+public class LinkModeToBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not LinkMode mode)
+            throw new InvalidOperationException("The target must be a LinkMode enum");
+
+        return mode == LinkMode.FileMode; // 如果是 FileMode 则返回 true，否则返回 false
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool b)
+            throw new InvalidOperationException("The value must be a boolean");
+
+        return b ? LinkMode.FileMode : LinkMode.FolderMode; // 如果是 true 则返回 FileMode，否则返回 FolderMode
+    }
+}
+
+public class LinkModeToBoolReverseConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not LinkMode mode)
+            throw new InvalidOperationException("The target must be a LinkMode enum");
+
+        return mode == LinkMode.FolderMode; // 如果是 FolderMode 则返回 true，否则返回 false
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool b)
+            throw new InvalidOperationException("The value must be a boolean");
+
+        return b ? LinkMode.FolderMode : LinkMode.FileMode; // 如果是 true 则返回 FolderMode，否则返回 FileMode
+    }
+}
+
+public class LinkModeToHintStringConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not LinkMode mode)
+            throw new InvalidOperationException("The target must be a LinkMode enum");
+        return mode == LinkMode.FileMode ? "原始文件路径" : "原始文件夹路径";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new InvalidOperationException("Converting back from hint string to Link Mode is invalid!");
+    }
+}
+
+public class MainWindowViewModel: INotifyPropertyChanged
+{
+    #region PrivateVariable
+
+    private LinkMode _linkMode = LinkMode.FileMode; // true for file mode, false for folder mode.
+    private string _linkPath = string.Empty;
+    private string _targetPath = string.Empty;
+    private string _executionResult = "等待执行";
+    private Brush _executionResultForeground = new SolidColorBrush(Colors.White);
+    private bool _isHardLink;
+    private bool _isJunction;
+
+    #endregion
+    
+    #region Property
+
+    public LinkMode LinkMode
+    {
+        get => _linkMode;
+        set
+        {
+            if (_linkMode == value) return;
+            _linkMode = value;
+            OnPropertyChanged(nameof(LinkMode));
+        }
+    }
+
+    public string LinkPath
+    {
+        get => _linkPath;
+        set
+        {
+            if (_linkPath == value) return;
+            _linkPath = value;
+            OnPropertyChanged(nameof(LinkPath));
+        }
+    }
+
+    public string TargetPath
+    {
+        get => _targetPath;
+        set
+        {
+            if (_targetPath == value) return;
+            _targetPath = value;
+            OnPropertyChanged(nameof(TargetPath));
+        }
+    }
+
+    public bool IsHardLink
+    {
+        get => _isHardLink;
+        set
+        {
+            if (_isHardLink == value) return;
+            _isHardLink = value;
+            OnPropertyChanged(nameof(IsHardLink));
+        }
+    }
+
+    public bool IsJunction
+    {
+        get => _isJunction;
+        set
+        {
+            if (_isJunction == value) return;
+            _isJunction = value;
+            OnPropertyChanged(nameof(IsJunction));
+        }
+    }
+
+    public string ExecutionResult
+    {
+        get => _executionResult;
+        set
+        {
+            if (_executionResult == value) return;
+            _executionResult = value;
+            OnPropertyChanged(nameof(ExecutionResult));
+        }
+    }
+
+    public Brush ExecutionResultForeground
+    {
+        get => _executionResultForeground;
+        set
+        {
+            if (_executionResultForeground == value) return;
+            _executionResultForeground = value;
+            OnPropertyChanged(nameof(ExecutionResultForeground));
+        }
+    }
+
+    #endregion
+    
+    #region PropertyChangedHandler
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+}
